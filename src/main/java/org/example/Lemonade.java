96e8a9b28ff29638bf1feaa4de966a683fc83878
Jalen McBride <jamcbride131@gmail.com>
2025-09-17 22:08:59 -0500

    Completed Exercise

--- src/main/java/org/example/Lemonade.java
+++ src/main/java/org/example/Lemonade.java
@@ -48,6 +48,14 @@ public class Lemonade {
 
     // Create a method called mix that takes two Lemonade objects and returns a new Lemonade object with
     // the sum of the lemons, sugar, and ice from the two Lemonade objects.
+    public static Lemonade mix(Lemonade lemonade1, Lemonade lemonade2){
+        int lemonCount = lemonade1.lemons + lemonade2.lemons;
+        int sugarCount = lemonade1.sugar + lemonade2.sugar;
+        int iceCount = lemonade1.ice + lemonade2.ice;
+
+       return new Lemonade(lemonCount, sugarCount, iceCount);
+
+    }
     //
     // HINT: You can get the counts from the two Lemonade objects using their get methods, e.g.:
     // int newLemonadeLemons = lemonade1.getLemons() + lemonade2.getLemons();
